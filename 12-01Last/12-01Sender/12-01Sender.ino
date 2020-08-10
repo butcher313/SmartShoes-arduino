c1881d7cc2ef1da43f059b776457c9b2d4ddf0bc
butcher313 <40017192+butcher313@users.noreply.github.com>
2020-08-10 19:42:11 +0900

    Update 12-01Sender.ino

--- 12-01Last/12-01Sender/12-01Sender.ino
+++ 12-01Last/12-01Sender/12-01Sender.ino
@@ -1,3 +1,6 @@
+/*
+지그비를 통해 수신부 아두이노에 데이터를 전송하는 아두이노 코드
+*/
 
 void setup() {
   // put your setup code here, to run once:
@@ -8,12 +11,14 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
 
+  // 압력센서들로부터 값을 읽어들인다. 
   int val0=analogRead(A0);
   int val1=analogRead(A1);
   int val2=analogRead(A2);
 
   int total = val0 + val1 + val2;
-
+  
+  // 데이터를 수신하는 아두이노로부터 신호가 오면 압력 데이터를 전송
   if(Serial.available()){
     byte data = Serial.read();
     if(data == 'A'){
